ebca256deabcc4f4dfe831fddf8f2803f3aeed76
Cristo Suárez <cristobits0101@gmail.com>
2025-09-24 23:15:12 +0100

    Update

--- auth/src/main/java/com/cryfirock/auth/service/services/IUserService.java
+++ auth/src/main/java/com/cryfirock/auth/service/services/IUserService.java
@@ -6,20 +6,21 @@ import java.util.Optional;
 import com.cryfirock.auth.service.entities.User;
 
 /**
- * ===============================================================
+ * ==============================================================
  * Paso 7.1: Interfaz de servicio para la gestión de usuarios
- * ===============================================================
+ * ==============================================================
  */
+
 // Define las operaciones disponibles para la gestión de usuarios
 public interface IUserService {
 
     /**
-     * ===========================================================
+     * ==========================================================
      * Paso 7.2: Métodos de validación
-     * ===========================================================
+     * ==========================================================
      */
 
-    // Métodos de validación de existencia de atributos de usuario
+    // Validación de existencia de atributos de usuario
     boolean existsByEmail(String email);
 
     boolean existsByPhoneNumber(String phoneNumber);
@@ -27,9 +28,9 @@ public interface IUserService {
     boolean existsByUsername(String username);
 
     /**
-     * ===========================================================
+     * ==========================================================
      * Paso 7.3: Métodos CRUD
-     * ===========================================================
+     * ==========================================================
      */
 
     // Operaciones CRUD de la entidad User
