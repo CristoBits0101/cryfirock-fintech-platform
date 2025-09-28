1af039a8815e9dced82f5f25c70357212461e86a
Cristo Suárez <cristobits0101@gmail.com>
2025-09-28 15:37:11 +0100

    Refactoring

--- auth/src/main/java/com/cryfirock/auth/service/model/AccountStatus.java
+++ auth/src/main/java/com/cryfirock/auth/service/model/AccountStatus.java
@@ -2,7 +2,9 @@ package com.cryfirock.auth.service.model;
 
 // Estados permitidos en las cuentas de usuario
 public enum AccountStatus {
+    PENDING,
     ACTIVE,
     SUSPENDED,
+    BANNED,
     CLOSED
 }
