6717cc89bdf43b4b2e430c4d638fe6da904909b3
Cristo Suárez <cristobits0101@gmail.com>
2025-09-28 07:06:35 +0100

    Refactoring

--- auth/src/main/java/com/cryfirock/auth/service/mapper/UserMapper.java
+++ auth/src/main/java/com/cryfirock/auth/service/mapper/UserMapper.java
@@ -13,17 +13,21 @@ import com.cryfirock.auth.service.entity.User;
 
 /**
  * ==========================================================================================================================================================
- * Paso 12.1:
+ * Paso 12.1: Mapper de actualización parcial de DTO a Entidad
  * ==========================================================================================================================================================
  */
 
 // MapStruct crea algo como UserMapperImpl automáticamente
+// Evita escribir setters o constructores manuales para actualizar atributos
+// Aplica solo los campos no nulos del DTO
+// La implementación UserMapperImpl la genera MapStruct en compilación
+// Spring la inyecta como bean
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
 
     /**
      * ======================================================================================================================================================
-     * Paso 12.2:
+     * Paso 12.2: Métodos
      * ======================================================================================================================================================
      */
 
