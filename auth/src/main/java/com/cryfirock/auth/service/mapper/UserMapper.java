3017e8903f45bb1486737d2945e72f0a1b2b1112
Cristo Suárez <cristobits0101@gmail.com>
2025-09-28 03:44:16 +0100

    Refactoring

--- auth/src/main/java/com/cryfirock/auth/service/mapper/UserMapper.java
+++ auth/src/main/java/com/cryfirock/auth/service/mapper/UserMapper.java
@@ -1,13 +1,25 @@
 package com.cryfirock.auth.service.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
 import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
 
 import com.cryfirock.auth.service.dto.UserUpdateDto;
 import com.cryfirock.auth.service.entity.User;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+
+    @BeanMapping(ignoreByDefault = false)
+    @Mappings({
+            @Mapping(target = "roles", ignore = true),
+            @Mapping(target = "admin", ignore = true),
+            @Mapping(target = "passwordHash", ignore = true),
+            @Mapping(target = "id", ignore = true)
+    })
     void update(@MappingTarget User target, UserUpdateDto dto);
 }
