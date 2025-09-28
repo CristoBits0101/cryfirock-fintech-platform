d41ba678438c4618cc7aa37e98b2c44911a63bdb
Cristo Suárez <cristobits0101@gmail.com>
2025-09-28 02:33:34 +0100

    Refactoring

--- auth/src/main/java/com/cryfirock/auth/service/dto/UserUpdateDto.java
+++ auth/src/main/java/com/cryfirock/auth/service/dto/UserUpdateDto.java
@@ -2,20 +2,22 @@ package com.cryfirock.auth.service.dto;
 
 import java.time.LocalDate;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
+// Dto con los datos de actualización de usuario mínimos
 public record UserUpdateDto(
-        @Size(max = 50) String givenName,
-        @Size(max = 50) String familyName,
-        LocalDate dob,
-
-        @Email @Size(max = 100) String email,
-        @Size(max = 20) String phoneNumber,
-        @Size(max = 255) String address,
-
-        @Size(min = 1, max = 50) String username,
-
-        String passwordHash,
-
-        Boolean enabled) {
+                // Identidad
+                @Size(max = 50) String givenName,
+                @Size(max = 50) String familyName,
+                LocalDate dob,
+                // Contacto
+                @Email @Size(max = 100) String email,
+                @Size(max = 20) String phoneNumber,
+                @Size(max = 255) String address,
+                // Cuenta
+                @Size(min = 1, max = 50) String username,
+                String passwordHash,
+                // Acceso
+                Boolean enabled) {
 }
