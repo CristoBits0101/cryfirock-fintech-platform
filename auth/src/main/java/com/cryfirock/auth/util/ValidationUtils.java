57f7944b64c6cb061880cc88a4707974415df1da
Cristo Suárez <cristobits0101@gmail.com>
2025-10-29 18:24:52 +0000

    Refactor

--- auth/src/main/java/com/cryfirock/auth/util/ValidationUtils.java
+++ auth/src/main/java/com/cryfirock/auth/util/ValidationUtils.java
@@ -1,24 +1,23 @@
 package com.cryfirock.auth.util;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 @Component
 public class ValidationUtils {
   public ResponseEntity<?> reportIncorrectFields(BindingResult result) {
-    Map<String, String> requestErrors = new HashMap<>();
-
-    result
-        .getFieldErrors()
-        .forEach(err -> {
-          requestErrors.put(err.getField(), err.getDefaultMessage());
-        });
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(requestErrors);
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(result
+            .getFieldErrors()
+            .stream()
+            .collect(Collectors.toMap(
+                FieldError::getField,
+                FieldError::getDefaultMessage)));
   }
 }
