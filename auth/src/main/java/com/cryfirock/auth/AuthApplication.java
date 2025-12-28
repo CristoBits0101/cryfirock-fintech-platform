47fd0c03ed062e94c0cd5a46e73159fd2df3582f
Cristo Suárez <cristobits0101@gmail.com>
2025-12-28 14:50:39 +0000

    Enhance localization by translating properties and adding comments for clarity in AuthApplication, application.properties, and messages.properties

--- auth/src/main/java/com/cryfirock/auth/AuthApplication.java
+++ auth/src/main/java/com/cryfirock/auth/AuthApplication.java
@@ -4,10 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * 001: Configuración principal de la aplicación de autenticación.
+ * 
+ * @EnableAspectJAutoProxy: Habilita proxies AOP para interceptar métodos.
+ * @SpringBootApplication: Marca esta clase como aplicación Spring Boot.
+ */
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class AuthApplication {
+
+  /**
+   * Punto de entrada principal de la aplicación Spring Boot.
+   * 
+   * @param args: Argumentos de línea de comandos.
+   */
   public static void main(String[] args) {
     SpringApplication.run(AuthApplication.class, args);
   }
+
 }
