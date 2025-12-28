35602b2252dd178060eb9904a6fb0cfbed5a263a
Cristo Suárez <cristobits0101@gmail.com>
2025-12-28 15:10:39 +0000

    Remove redundant comment in Audit class for improved clarity

--- auth/src/main/java/com/cryfirock/auth/entity/Audit.java
+++ auth/src/main/java/com/cryfirock/auth/entity/Audit.java
@@ -13,7 +13,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * 004: Esta clase puede incrustarse en otra y autogenera métodos.
  * @AllArgsConstructor: Genera constructor con todos los atributos.
  * @NoArgsConstructor: Genera constructor sin argumentos.
  * @Data: Genera getters, setters, toString, equals y hashCode.
