079b2029fc6148a08f09a870252604759d1b4b1a
Cristo Suárez <cristobits0101@gmail.com>
2026-01-04 17:46:29 +0000

    Add UserFeignClient interface for Feign client integration

--- oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
+++ oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
@@ -1,7 +1,16 @@
 package com.cryfirock.oauth2.provider.client;
 
+import java.util.Optional;
+
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.validation.constraints.NotNull;
 
 @FeignClient(url = "localhost:8082")
 public interface UserFeignClient {
+
+    @GetMapping("/{id}")
+    Optional<User> findById(@NotNull Long id);
+
 }
---
+++ oauth2/src/main/java/com/cryfirock/oauth2/provider/model/User.java
@@ -0,0 +1,5 @@
+package com.cryfirock.oauth2.provider.model;
+
+public class User {
+    
+}
