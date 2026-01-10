a19be08236711c3f17d7fc8178d51143b0a44509
Cristo Suárez <cristobits0101@gmail.com>
2026-01-10 20:58:14 +0000

    Fix application.properties formatting and update UserFeignClient URL

--- oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
+++ oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
@@ -9,7 +9,7 @@ import com.cryfirock.oauth2.provider.model.User;
 
 import jakarta.validation.constraints.NotNull;
 
-@FeignClient(url = "localhost:8082")
+@FeignClient(url = "localhost:8082, ")
 public interface UserFeignClient {
     @GetMapping("/{id}")
     Optional<User> findById(@NotNull Long id);
