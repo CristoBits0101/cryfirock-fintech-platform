f8335d254c42aea1183e456b13eab5272487ad63
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 22:26:59 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/AuthApplication.java
+++ auth/src/main/java/com/cryfirock/auth/AuthApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * 1. Principal clase de la aplicación Auth.
+ * 2. Anotación @EnableAspectJAutoProxy habilita el soporte para AspectJ.
+ * 3. Anotación @SpringBootApplication indica que es una aplicación Spring Boot.
+ * 4. Método main inicia la aplicación.
+ * 5. Usa SpringApplication.run para lanzar la aplicación con los argumentos proporcionados.
+ */
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class AuthApplication {
