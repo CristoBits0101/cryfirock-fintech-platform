3d1d71a2897c8902f8a1f3ba15ce8034f83e4b45
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 21:28:21 +0000

    document: code logic

--- auth/src/main/java/com/cryfirock/auth/dto/UserLoginDto.java
+++ auth/src/main/java/com/cryfirock/auth/dto/UserLoginDto.java
@@ -3,6 +3,19 @@ package com.cryfirock.auth.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+/**
+ * 1. DTO para login de usuario.
+ * 2. Record:
+ *      - Es inmutable.
+ *      - Genera getters, toString, hashCode y equals.
+ *      - No requiere constructor ni setters.
+ *      - Validación más directa:
+ *          - @NotBlank: No puede ser nulo ni vacío.
+ *          - @Size: Define el rango de caracteres.
+ *      - Thread-safe no cambia y no te preocupa concurrencia ni estados raros.
+ * 3. username: No puede ser nulo y debe tener entre 1 y 50 caracteres.
+ * 4. password: No puede ser nulo y debe tener entre 6 y 128 caracteres.
+ */
 public record UserLoginDto(
                 @NotBlank @Size(min = 1, max = 50) String username,
                 @NotBlank @Size(min = 6, max = 128) String password) {
--- auth/src/main/java/com/cryfirock/auth/dto/UserUpdateDto.java
+++ auth/src/main/java/com/cryfirock/auth/dto/UserUpdateDto.java
@@ -7,6 +7,27 @@ import com.cryfirock.auth.model.AccountStatus;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
+/**
+ * 1. DTO para actualizar un usuario.
+ * 2. Record:
+ *      - Es inmutable.
+ *      - Genera getters, toString, hashCode y equals.
+ *      - No requiere constructor ni setters.
+ *      - Validación más directa:
+ *          - @Size: Define el rango de caracteres.
+ *          - @Email: Define el formato de correo electrónico.
+ *      - Thread-safe no cambia y no te preocupa concurrencia ni estados raros.
+ * 3. givenName: Nombre dado.
+ * 4. familyName: Apellido familiar.
+ * 5. dob: Fecha de nacimiento.
+ * 6. email: Correo electrónico.
+ * 7. phoneNumber: Número de teléfono.
+ * 8. address: Dirección.
+ * 9. username: Nombre de usuario.
+ * 10. passwordHash: Hash de la contraseña.
+ * 11. admin: Indica si es administrador.
+ * 12. enabled: Indica si el usuario está habilitado.
+ */
 public record UserUpdateDto(
                 @Size(max = 50) String givenName,
                 @Size(max = 50) String familyName,
