1ae54280de6c606a4c9e68ea09b78d01486b7fa6
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 12:53:11 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/entity/Audit.java
+++ auth/src/main/java/com/cryfirock/auth/entity/Audit.java
@@ -12,10 +12,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+// Genera un constructor con todos los argumentos.
 @AllArgsConstructor
+// Genera un constructor sin argumentos.
 @NoArgsConstructor
+// Genera getters, setters, toString, hashCode y equals.
 @Data
+// Marca la clase como un componente embebible en una entidad JPA.
 @Embeddable
+// Sobrescribe las columnas para mapear los atributos a las columnas de la tabla.
 @AttributeOverrides({
     @AttributeOverride(name = "createdAt", column = @Column(name = "created_at", nullable = false, updatable = false)),
     @AttributeOverride(name = "updatedAt", column = @Column(name = "updated_at")),
