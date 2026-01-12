7022d601dd36f1a85f368ce314ddb217ab8efadb
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 01:39:33 +0000

    docs: añade comentarios aclaratorios

--- oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
+++ oauth2/src/main/java/com/cryfirock/oauth2/provider/client/UserFeignClient.java
@@ -4,9 +4,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// 
+// Interfaz para el cliente Feign que se comunica con el microservicio de usuarios.
+// Contrato de implementación para las clases que la implementen.
+// Obliga a cumplir la convención de nombres y responsabilidades definidas.
 @FeignClient(name = "msvc-users")
 public interface UserFeignClient {
+    /**
+     * Llamada al endpoint para verificar si un correo electrónico ya existe.
+     * 
+     * @param email el correo electrónico a verificar.
+     * @return true si el email ya existe y false en caso contrario.
+     */
     @GetMapping("/api/validations/exists/email/{email}")
-    Boolean checkEmailExists(@PathVariable("email") String email);
+    Boolean checkEmailExists(@PathVariable String email);
 }
