8f35dde82cb130ee2e3b9fb518ad42ad30f418b9
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 15:32:03 +0000

    docs: añade comentarios aclaratorios

--- auth/src/test/java/com/cryfirock/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/cryfirock/auth/AuthApplicationTests.java
@@ -3,10 +3,14 @@ package com.cryfirock.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Anotación para indicar que es una prueba de Spring Boot.
+/**
+ * 1. Clase de pruebas de la aplicación de autenticación.
+ * 2. Anotación para indicar que es una prueba de Spring Boot.
+ * 3. Prueba de carga del contexto de Spring Boot.
+ * 4. Utiliza JUnit 5 para las pruebas.
+ */
 @SpringBootTest
 class AuthApplicationTests {
-  // Pruebas para verificar que el contexto de la aplicación se carga.
   @Test
   void contextLoads() {
   }
