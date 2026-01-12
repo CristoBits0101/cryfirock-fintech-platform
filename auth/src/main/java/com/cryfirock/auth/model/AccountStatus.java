39a1506fb5dfc503e28e50e89b1a1e40359455c2
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 14:18:54 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/model/AccountStatus.java
+++ auth/src/main/java/com/cryfirock/auth/model/AccountStatus.java
@@ -1,6 +1,17 @@
 package com.cryfirock.auth.model;
 
+/**
+ * 1. Enumera los posibles estados de una cuenta de usuario.
+ * 2. Proporciona una forma segura de manejar los estados de la cuenta.
+ */
 public enum AccountStatus {
+  /**
+   * 1. La cuenta está pendiente de activación.
+   * 2. La cuenta está activa y en buen estado.
+   * 3. La cuenta está suspendida temporalmente.
+   * 4. La cuenta está baneada permanentemente.
+   * 5. La cuenta está cerrada por el usuario.
+   */
   PENDING,
   ACTIVE,
   SUSPENDED,
--- auth/src/main/java/com/cryfirock/auth/model/Error.java
+++ auth/src/main/java/com/cryfirock/auth/model/Error.java
@@ -4,8 +4,18 @@ import java.util.Date;
 
 import lombok.Data;
 
+/**
+ * 1. Clase que representa un error en la aplicación.
+ * 2. Genera getters, setters, toString, hashCode y equals.
+ */
 @Data
 public class Error {
+  /**
+   * 1. Mensaje descriptivo del error.
+   * 2. Error específico ocurrido.
+   * 3. Código de estado HTTP asociado al error.
+   * 4. Fecha y hora en que ocurrió el error.
+   */
   private String message;
   private String error;
   private int status;
