b9d4ae3eb32ae2e7c18955302498f65567e59bf0
Cristo Suárez <cristobits0101@gmail.com>
2026-01-12 22:42:47 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/exception/UserNotFoundException.java
+++ auth/src/main/java/com/cryfirock/auth/exception/UserNotFoundException.java
@@ -1,6 +1,15 @@
 package com.cryfirock.auth.exception;
 
+/**
+ * 1. Excepción personalizada para indicar que un usuario no fue encontrado.
+ * 2. Extiende RuntimeException para ser una excepción no verificada.
+ */
 public class UserNotFoundException extends RuntimeException {
+  /**
+   * 1. Constructor que acepta un mensaje de error.
+   * 
+   * @param message Mensaje de error detallando la excepción.
+   */
   public UserNotFoundException(String message) {
     super(message);
   }
