d5704ad86a54da1615d4746b576eda321ac85249
Cristo Suárez <cristobits0101@gmail.com>
2026-01-13 00:09:55 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/config/AppConfig.java
+++ auth/src/main/java/com/cryfirock/auth/config/AppConfig.java
@@ -12,14 +12,28 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**                                                           
+ * 1. Componente de configuración de la aplicación.
+ * 2. Escanea los paquetes especificados para detectar componentes de servicios y validadores.
+ */
 @Configuration
 @ComponentScan(basePackages = { "com.cryfirock.auth.service", "com.cryfirock.auth.validation" })
 public class AppConfig implements WebMvcConfigurer {
+  /**
+   * 1. Interceptor para operaciones de usuario.
+   * 2. Se inyecta mediante Autowired y Qualifier.
+   * 3. Se utiliza para interceptar solicitudes a rutas específicas relacionadas con usuarios.
+   */
   @Autowired
   @Qualifier("userOperationsInterceptor")
   @NonNull
   private HandlerInterceptor userOperationsInterceptor;
 
+  /**
+   * 1. Configura el interceptor userOperationsInterceptor para la aplicación web.
+   * 2. Intercepta todas las solicitudes a rutas que coinciden con /api/users/**
+   * 3. Excluye la ruta /api/users de la interceptación.
+   */
   @Override
   public void addInterceptors(@NonNull InterceptorRegistry registry) {
     registry
