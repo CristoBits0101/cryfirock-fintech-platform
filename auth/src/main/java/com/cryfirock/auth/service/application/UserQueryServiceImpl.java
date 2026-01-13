f8231578b3bd83e486ba4d29b05adcf5544c6621
Cristo Suárez <cristobits0101@gmail.com>
2026-01-13 11:03:14 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/service/application/UserQueryServiceImpl.java
+++ auth/src/main/java/com/cryfirock/auth/service/application/UserQueryServiceImpl.java
@@ -5,14 +5,28 @@ import org.springframework.stereotype.Service;
 import com.cryfirock.auth.repository.JpaUserRepository;
 import com.cryfirock.auth.service.contract.IUserQueryService;
 
+/**
+ * 1. Se ocupa de las consultas relacionadas con los usuarios.
+ * 2. Implementa la interfaz IUserQueryService.
+ * 3. Utiliza JpaUserRepository para interactuar con la base de datos.
+ * 4. Proporciona métodos para verificar la existencia de datos.
+ */
 @Service
 public class UserQueryServiceImpl implements IUserQueryService {
+  // Repositorio JPA para usuarios
   private final JpaUserRepository userRepository;
 
+  // Constructor que inyecta el repositorio de usuarios
   public UserQueryServiceImpl(JpaUserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
+  /**
+   * 1. Verifica si un correo electrónico ya está registrado.
+   * 2. Verifica si un número de teléfono ya está registrado.
+   * 3. Verifica si un nombre de usuario ya está registrado.
+   * 4. Utiliza métodos del repositorio para realizar las verificaciones.
+   */
   @Override
   public boolean existsByEmail(String email) {
     return userRepository.existsByEmail(email);
