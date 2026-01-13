9c3f40a62953bf4959066e6308cfc068943ec064
Cristo Suárez <cristobits0101@gmail.com>
2026-01-13 10:52:55 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/helper/RolesHelper.java
+++ auth/src/main/java/com/cryfirock/auth/helper/RolesHelper.java
@@ -14,19 +14,51 @@ import com.cryfirock.auth.repository.JpaRoleRepository;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 1. Clase auxiliar para asignar roles a usuarios.
+ * 2. Proporciona métodos para determinar y asignar roles.
+ */
 @Component
 @Setter
 @Getter
 public class RolesHelper {
+  /**
+   * 1. Roles predefinidos en el sistema.
+   * 2. ROLE_USER: Rol básico para todos los usuarios.
+   * 3. ROLE_ADMIN: Rol con privilegios administrativos.
+   * 4. Se utilizan para controlar el acceso y las funcionalidades.
+   * 5. Static para compartir atributos entre instancias.
+   * 6. Final para evitar modificaciones en tiempo de ejecución.
+   */
   private static final String ROLE_USER = "ROLE_USER";
   private static final String ROLE_ADMIN = "ROLE_ADMIN";
   private final JpaRoleRepository roleRepository;
 
+  /**
+   * Constructor de la clase RolesHelper.
+   * 
+   * @param roleRepository Repositorio de roles para acceder a los datos de roles.
+   */
   public RolesHelper(JpaRoleRepository roleRepository) {
     this.roleRepository = roleRepository;
   }
 
+  /**
+   * Asigna roles a un usuario basado en su estado administrativo.
+   * 
+   * @param user Usuario al que se le asignarán los roles.
+   * @return Lista de roles asignados al usuario.
+   */
   public List<Role> assignRoles(User user) {
+    /**
+     * 1. Verifica si el usuario es administrador.
+     * 2. Si es administrador asigna ROLE_USER y ROLE_ADMIN.
+     * 3. Si no es administrador asigna solo ROLE_USER.
+     * 4. Utiliza Streams para mapear los nombres de roles a objetos Role.
+     * 5. Lanza una excepción si un rol no se encuentra en el repositorio.
+     * 6. Devuelve una lista de roles asignados al usuario.
+     * 7. Converte el Stream resultante en una ArrayList.
+     */
     return (user.isAdmin()
         ? Stream.of(ROLE_USER, ROLE_ADMIN)
         : Stream.of(ROLE_USER))
