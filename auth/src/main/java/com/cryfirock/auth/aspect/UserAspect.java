6995e9f73463585ba39cbd0f204018dc627f0d18
Cristo Suárez <cristobits0101@gmail.com>
2026-01-13 13:39:19 +0000

    docs: añade comentarios aclaratorios

--- auth/src/main/java/com/cryfirock/auth/aspect/UserAspect.java
+++ auth/src/main/java/com/cryfirock/auth/aspect/UserAspect.java
@@ -14,43 +14,93 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+/**
+ * 1. Aspecto para el registro de datos métodos en UserServiceImpl.
+ * 2. Proporciona logging antes y después de la ejecución de algunos métodos.
+ * 3. Maneja logging para ejecuciones exitosas y excepciones.
+ * 4. Utiliza AOP de Spring para interceptar llamadas a métodos.
+ */
 @Aspect
 @Component
 public class UserAspect {
+  // 1. Logger para registrar mensajes de log.
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+  /**
+   * 1. Log antes de ejecutar métodos de la clase UserServiceImpl.
+   * 2. Registra el nombre del método y sus argumentos.
+   * 
+   * @param joinPoint Información del punto de unión.
+   */
   @Before("execution(* com.cryfirock.auth.service.UserServiceImpl.*(..))")
   public void loggerBefore(JoinPoint joinPoint) {
-    String method = joinPoint.getSignature().getName();
-    String args = Arrays.toString(joinPoint.getArgs());
-    logger.info("Antes de ejecutar: " + method + " con argumentos " + args);
+    // Obtener el nombre del método y sus argumentos.
+    String methodName = joinPoint.getSignature().getName();
+    String methodArgs = Arrays.toString(joinPoint.getArgs());
+    // Mostrar el log antes de la ejecución del método.
+    logger.info("Antes de ejecutar: " + methodName + " con argumentos " + methodArgs);
   }
 
+  /**
+   * 1. Log después de ejecutar métodos de la clase UserServiceImpl.
+   * 2. Registra el nombre del método y sus argumentos.
+   * 
+   * @param joinPoint Información del punto de unión.
+   */
   @After("execution(* com.cryfirock.auth.service.UserServiceImpl.*(..))")
   public void loggerAfter(JoinPoint joinPoint) {
-    String method = joinPoint.getSignature().getName();
-    String args = Arrays.toString(joinPoint.getArgs());
-    logger.info("Después de ejecutar: " + method + " con argumentos " + args);
+    // Obtener el nombre del método y sus argumentos.
+    String methodName = joinPoint.getSignature().getName();
+    String methodArgs = Arrays.toString(joinPoint.getArgs());
+    // Mostrar el log después de la ejecución del método.
+    logger.info("Después de ejecutar: " + methodName + " con argumentos " + methodArgs);
   }
 
+  /**
+   * 1. Log después de una ejecución exitosa de métodos en UserServiceImpl.
+   * 2. Registra el nombre del método y sus argumentos.
+   * 
+   * @param joinPoint Información del punto de unión.
+   */
   @AfterReturning("execution(* com.cryfirock.auth.service.UserServiceImpl.*(..))")
   public void loggerAfterReturning(JoinPoint joinPoint) {
+    // Obtener el nombre del método y sus argumentos.
     String method = joinPoint.getSignature().getName();
     String args = Arrays.toString(joinPoint.getArgs());
+    // Mostrar el log después de una ejecución exitosa.
     logger.info("Después de una ejecución exitosa: " + method + " con argumentos " + args);
   }
 
+  /**
+   * 1. Log después de que un método en UserServiceImpl lanza una excepción.
+   * 2. Registra el nombre del método y sus argumentos.
+   * 
+   * @param joinPoint
+   */
   @AfterThrowing("execution(* com.cryfirock.auth.service.UserServiceImpl.*(..))")
   public void loggerAfterThrowing(JoinPoint joinPoint) {
+    // Obtener el nombre del método y sus argumentos.
     String method = joinPoint.getSignature().getName();
     String args = Arrays.toString(joinPoint.getArgs());
+    // Mostrar el log después de una excepción.
     logger.error("Después de una excepción en: " + method + " con argumentos " + args);
   }
 
+  /**
+   * 1. Log alrededor de la ejecución de métodos en UserServiceImpl.
+   * 2. Registra el nombre del método y sus argumentos.
+   * 3. Registra el valor retornado y cualquier excepción lanzada.
+   * 
+   * @param joinPoint Información del punto de unión.
+   * @return El valor retornado por el método interceptado.
+   * @throws Throwable Excepciones lanzadas por el método interceptado.
+   */
   @Around("execution(* com.cryfirock.auth.service.UserServiceImpl.*(..))")
   public Object loggerAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    // Obtener el nombre del método y sus argumentos.
     String method = joinPoint.getSignature().getName();
     String args = Arrays.toString(joinPoint.getArgs());
+    // Mostrar el log alrededor de la ejecución del método.
     try {
       logger.info("Entrando al método " + method + "() con parámetros " + args);
       Object result = joinPoint.proceed();
