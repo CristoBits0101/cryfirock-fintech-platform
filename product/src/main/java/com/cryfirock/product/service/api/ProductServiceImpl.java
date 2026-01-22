4e20694c89c05f733bf5687f7d528399f9ab029c
Cristo Suárez <cristobits0101@gmail.com>
2026-01-22 12:24:42 +0000

    refactoring: comments

--- product/src/main/java/com/cryfirock/product/service/api/ProductServiceImpl.java
+++ product/src/main/java/com/cryfirock/product/service/api/ProductServiceImpl.java
@@ -2,13 +2,11 @@ package com.cryfirock.product.service.api;
 
 import org.springframework.stereotype.Service;
 
-import com.cryfirock.product.service.impl.IProductService;
-
 @Service
 public class ProductServiceImpl {
-    private final IProductService productService;
+    // private final IProductService productService;
 
-    public ProductServiceImpl(IProductService service) {
-        productService = service;
-    }
+    // public ProductServiceImpl(IProductService service) {
+    // productService = service;
+    // }
 }
