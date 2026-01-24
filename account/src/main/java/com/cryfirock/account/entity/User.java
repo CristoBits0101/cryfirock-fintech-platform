b9e1e9a503eeb6931977dc106b4b7756687129b9
Cristo Suárez <cristobits0101@gmail.com>
2026-01-24 22:12:56 +0000

    add: entities

--- account/src/main/java/com/cryfirock/account/entity/User.java
+++ account/src/main/java/com/cryfirock/account/entity/User.java
@@ -1,8 +1,12 @@
 package com.cryfirock.account.entity;
 
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@EnableAspectJAutoProxy
+@Entity
 public class User {
-    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
