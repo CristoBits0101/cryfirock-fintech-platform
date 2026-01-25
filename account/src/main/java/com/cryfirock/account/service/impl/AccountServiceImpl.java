a0c87f79d6d39e095a80fa332cc70fa87c7e1b75
Cristo Suárez <cristobits0101@gmail.com>
2026-01-25 14:35:42 +0000

    feat: implement account service with CRUD operations and relationship management for accounts, users, and products.

--- account/src/main/java/com/cryfirock/account/service/impl/AccountServiceImpl.java
+++ account/src/main/java/com/cryfirock/account/service/impl/AccountServiceImpl.java
@@ -1,10 +1,8 @@
 package com.cryfirock.account.service.impl;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
